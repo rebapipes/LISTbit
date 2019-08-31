1137d7cf95af9854fca6975df4edb61e865e13f7
Reba Pipes <rebablackbox@gmail.com>
2019-08-31 18:03:15 +0000

    task creation API

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -5,15 +5,10 @@ RSpec.describe TasksController, type: :controller do
     it "should list the tasks in the database" do
       task1 = FactoryBot.create(:task)
       task2 = FactoryBot.create(:task)
-      task1.update_attributes(title: "Something else")
       get :index
-      expect(response).to have_http_status :success
+      assert_response :success
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value.count).to eq(2)
-      response_ids = response_value.collect do |task|
-        task["id"]
-      end
-      expect(response_ids).to eq([task1.id, task2.id])
     end
   end
 
@@ -27,4 +22,13 @@ RSpec.describe TasksController, type: :controller do
     end
   end
 
+  describe "tasks#create" do
+    it "should allow new tasks to be created" do
+      post :create, params: {task: {title: "Fix things"}}
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq("Fix things")
+      expect(Task.last.title).to eq("Fix things")
+    end
+  end
 end
